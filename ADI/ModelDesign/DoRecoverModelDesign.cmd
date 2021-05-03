1b22b8acd509bc760ecc671fa5df21a917be032d
Mariusz <mpostol@cas.eu>
2021-05-03 18:06:35 +0200

    Integration tests using asp 6.30 #18 Generate MachineVision ModelDesign format from NodeSet2 format #7
    - tested DI
    - Tested ADI
    - Tested MachineVision

--- ADI/ModelDesign/DoRecoverModelDesign.cmd
+++ ADI/ModelDesign/DoRecoverModelDesign.cmd
@@ -1,1 +1,1 @@
-%1\asp "..\..\DI\Opc.Ua.Di.NodeSet2.xml" "..\Opc.Ua.Adi.NodeSet2.xml" -e "Opc.Ua.Adi.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/ADI/" >>Opc.Ua.Adi.NodeSet2.xml.log
+%1\asp "..\..\DI\Opc.Ua.Di.NodeSet2.xml" "..\Opc.Ua.Adi.NodeSet2.xml" -e "Opc.Ua.Adi.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/ADI/" >Opc.Ua.Adi.NodeSet2.xml.log
--- MachineVision/ModelDesign/DoRecoverModelDesign.cmd
+++ MachineVision/ModelDesign/DoRecoverModelDesign.cmd
@@ -1,1 +1,2 @@
-%1\asp "..\Opc.Ua.MachineVision.NodeSet2.xml" -e "Opc.Ua.MachineVision.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/MachineVision"
+::Recovering ModelDesign from UANodeSet OPC UA Model Information
+%1\asp "..\Opc.Ua.MachineVision.NodeSet2.xml" -e "Opc.Ua.MachineVision.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/MachineVision" >"Opc.Ua.MachineVision.Model.log"
