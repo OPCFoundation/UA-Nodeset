6a21936cb10cd21413ffca3e128fc444408d1cd6
Mariusz <mpostol@cas.eu>
2021-05-19 23:06:32 +0200

    Testing using publicly available models #8
    - preparing to the new version of the asp

--- DI/ModelDesign/DoRecoverModelDesign.cmd
+++ DI/ModelDesign/DoRecoverModelDesign.cmd
@@ -1,1 +1,2 @@
-%1\asp "..\Opc.Ua.Di.NodeSet2.xml" -e "Opc.Ua.Di.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/DI/" >Opc.Ua.Di.NodeSet2.xml.log
+%1\asp "..\Opc.Ua.Di.NodeSet2.xml" -e "Opc.Ua.Di.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/DI/"
+xcopy %1\asp.log asp.log
