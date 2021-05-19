c1bb4f28342c1f897dd7dc12cd3037bcd48cf973
Mariusz <mpostol@cas.eu>
2021-05-19 23:24:50 +0200

    Testing using publicly available models #8
    - tested against DI model.

--- DI/ModelDesign/DoRecoverModelDesign.cmd
+++ DI/ModelDesign/DoRecoverModelDesign.cmd
@@ -1,2 +1,4 @@
+:: %1 - location of the asp.exe 
+del %1\asp.log  /q
 %1\asp "..\Opc.Ua.Di.NodeSet2.xml" -e "Opc.Ua.Di.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/DI/"
-xcopy %1\asp.log asp.log
+xcopy %1\asp.log asp.log /y
