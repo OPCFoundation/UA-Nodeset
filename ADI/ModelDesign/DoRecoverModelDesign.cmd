f4125d0e06feedc18c8dbf9f66e448ad5358764b
Mariusz <mpostol@cas.eu>
2021-05-20 11:01:27 +0200

    Testing using publicly available models #8
    - testing ADI model

--- ADI/ModelDesign/DoRecoverModelDesign.cmd
+++ ADI/ModelDesign/DoRecoverModelDesign.cmd
@@ -1,1 +1,5 @@
-%1\asp "..\..\DI\Opc.Ua.Di.NodeSet2.xml" "..\Opc.Ua.Adi.NodeSet2.xml" -e "Opc.Ua.Adi.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/ADI/" >Opc.Ua.Adi.NodeSet2.xml.log
+:: %1 - location of the asp.exe 
+del %1\asp.log  /q
+%1\asp "..\..\DI\Opc.Ua.Di.NodeSet2.xml" "..\Opc.Ua.Adi.NodeSet2.xml" -e "Opc.Ua.Adi.Model.xml" -s XMLstylesheet -n "http://opcfoundation.org/UA/ADI/"
+xcopy %1\asp.log asp.log /y
+
